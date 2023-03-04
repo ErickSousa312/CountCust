f267feb41f6be13a6975230d421342877d057d3a
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2023-03-04 18:31:43 -0300

    add update reducer

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,44 @@
 
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import { useReducer, useState } from 'react'
+import { set } from 'mongoose'
+
+const reducer = (state, action)=>{
+  switch(action.type){
+    case "increment":
+      return {...state, count: state.count+1 }
+    case "showText":
+      return {...state, showText: !state.showText }  
+    default:
+      return "erro no swtich"
+  }
+}
 
 export default function Home() {
   
+    const[state, dispatch]= useReducer(reducer, {
+      count: 0,
+      showText: true,
+    })
+  
+    const handClick = ()=>{
+      dispatch({type: 'increment'})
+      dispatch({type: 'showText'})
+    }
+
+    const {count, showText} = state
+
   return (
     <>
-      <h1></h1>
-      <h1 className={styles.title}>Bem vindo a CountCost</h1>
+      <div className='app'>
+        <h1></h1>
+        <h3>{count}</h3>
+        <button onClick={handClick}>Clique</button>
+        {showText && <p> Bem vindo</p>}
+      </div>
+
     </>
   )
 }
 
-export async function getServerSideProps() {
-  const response = await fetch (`http://localhost:3000/person`)
-  const data = await response.json()
-  console.log(data._id)
-
-  return {props:{data}}
-}
